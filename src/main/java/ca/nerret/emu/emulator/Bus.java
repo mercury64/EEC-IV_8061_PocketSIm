3f8000029bc7002ef4b3e840e611f2ab1134245d
Warren E. White <wew@sidneybc.ca>
2021-03-15 19:59:21 -0700

    Adding BUS and CPUs

---
+++ src/main/java/ca/nerret/emu/emulator/Bus.java
@@ -0,0 +1,24 @@
+package ca.nerret.emu.emulator;
+
+public class Bus {
+
+    EECIV_8061 cpu;
+
+    public Bus()
+    {
+        // CPU
+        cpu.ConnectBus(this);
+
+        //Ram
+        // Fake RAM
+        byte ram[] = new byte[64 * 1024];
+
+    }
+
+    public byte read(short address, boolean readonly)
+    {
+
+        return;
+    }
+}
+ 
